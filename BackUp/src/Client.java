48761372b0e9f754f75091d9b76394c7501fc61e
SBylemans <sander_bylemans@hotmail.com>
2014-04-28 00:12:41 +0200

    fsdfsdf

---
+++ BackUp/src/Client.java
@@ -0,0 +1,125 @@
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.ArrayList;
+
+
+public class Client {
+	private DataInputStream serverToClient;
+	private Socket client;
+	private DataOutputStream clientToServer;
+	private byte[] buffer;
+
+	public Client(String name, int port){
+	    try {
+	    	client = new Socket(name, port);
+	        //receive response server
+	        serverToClient = new DataInputStream(client.getInputStream());
+	        //send message to server
+	        clientToServer = new DataOutputStream(client.getOutputStream());
+
+			 buffer = new byte[2048];
+	    }
+	    catch (IOException e) {
+	    }
+	}
+	
+	/**
+	 * Closes all connections
+	 */
+	public void stop(){
+		
+		try {
+			client.close();
+			serverToClient.close();
+			clientToServer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Sends all files and content of the files to the back-up server
+	 * @param filePath
+	 */
+	public void backUp(String filePath){
+		ArrayList<File> files = new ArrayList<File>();
+		boolean dir = false;
+		long length = 0;
+		try{
+			listf(filePath, files);
+			clientToServer.writeInt(files.size());
+			int total;
+			int count;
+			for(File fi : files){
+				dir = false;
+				if(fi.isDirectory()) dir = true;
+				clientToServer.writeInt(fi.getAbsolutePath().length());
+				System.out.println(fi.getAbsolutePath().length());
+				clientToServer.writeBytes(fi.getAbsolutePath());
+				length = fi.length();
+				clientToServer.writeLong(length);
+				clientToServer.writeBoolean(dir);
+				if(!dir){
+					FileInputStream fis = new FileInputStream(fi);
+					BufferedInputStream bis = new BufferedInputStream(fis);
+					total = 0;
+					count = 0;
+					while(total < length && (count = bis.read(buffer, 0, (int) Math.min(buffer.length, length-total))) > 0){
+						clientToServer.write(buffer,0,(int) Math.min(buffer.length, length-total));
+						total += count;
+					}
+					System.out.println(serverToClient.readUTF());
+					bis.close();
+					fis.close();
+					clientToServer.flush();
+				}
+			}
+
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Get all files, folders and subfolders in the file specified by directoryName, recursively.
+	 * @param directoryName
+	 * @param files
+	 * @param size
+	 */
+	private void listf(String directoryName, ArrayList<File> files) {
+	    File directory = new File(directoryName);
+	    if(directory.isFile()){
+	    	files.add(directory);
+	    }
+	    else{
+	    	File[] fList = directory.listFiles();
+	    	for (File file : fList) {
+	    		if(file.isDirectory() && file.listFiles().length == 0) files.add(file);
+	    		else{
+	    			if (file.isFile()) {
+	    				files.add(file);
+	    			} else if (file.isDirectory()) {
+	    				listf(file.getAbsolutePath(), files);
+	    			}
+	    		}
+	    	}
+	    }
+	}
+
+	public static void main(String[] args){
+		String name = args[0];
+		int port = Integer.parseInt(args[1]);
+		System.out.println("Name: " + name + " Port: " + port);
+		Client client = new Client(name, port);
+		File file = new File(args[2]);
+		if(file.exists())client.backUp(args[2]);
+		else System.out.println("File doesn't exist");
+		client.stop();
+		
+	}
+}
---
+++ BackUp/src/Server.java
@@ -0,0 +1,109 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class Server {
+
+	private ServerSocket server;
+	private Socket acceptingSocket;
+
+	private byte[] b = new byte[2048];
+	
+	public Server(int port){
+		try {
+			server = new ServerSocket(port);
+		} catch (IOException e) {
+			System.out.println("Try again");
+		}
+	}
+	
+	/**
+	 * Reads all files and puts them in the back-up folder. Also creates the appropriate dirs.
+	 */
+	public void run(){
+		BufferedInputStream buffer = null;
+		DataInputStream reader = null;
+		BufferedOutputStream out = null;
+		DataOutputStream writer = null;
+		int fileNameLength = 0;
+		int size = 0;
+		try {
+			acceptingSocket = server.accept();
+			buffer = new BufferedInputStream(acceptingSocket.getInputStream());
+			reader = new DataInputStream(buffer);
+			out = new BufferedOutputStream(acceptingSocket.getOutputStream());
+			writer = new DataOutputStream(out);
+			size = reader.readInt();
+			System.out.println("Size: " + size);
+		} catch (IOException e1) {
+		}
+		long length=0;
+		boolean dir  = false;
+		int t = 0;
+		String dirs = "";
+		File direcs;
+		File file;
+		int total;
+		int count;
+		String name = "";
+		for(int j = 0; j < size; j++){
+			try {
+				name = "";
+				fileNameLength = reader.readInt();
+				System.out.println("Filenamelength: " + fileNameLength);
+				total = 0;
+				count = 0;
+				//TODO
+				while(total < fileNameLength && (count = reader.read(b, 0, (int) Math.min(b.length, fileNameLength-total))) > 0){
+					name += new String(b,0,(int) Math.min(b.length, fileNameLength-total));
+					total += count;
+				}
+				String path = new String(name);
+				System.out.println("Path: " + path);
+				length = reader.readLong();
+				dir = reader.readBoolean();
+				System.out.println(dir);
+				path = "/backup" + path;
+				file = new File(path);
+				if(!dir){
+					t = file.getAbsolutePath().lastIndexOf("/");
+					dirs = file.getAbsolutePath().substring(0, t);
+					direcs = new File(dirs);
+					direcs.mkdirs();
+					FileOutputStream fos = new FileOutputStream(file);
+				    BufferedOutputStream bos = new BufferedOutputStream(fos);
+				    total = 0;
+					count = 0;
+					while(total < length && (count = reader.read(b, 0, (int) Math.min(b.length, length-total))) > 0){
+						bos.write(b,0,(int) Math.min(b.length, length-total));
+						total += count;
+					}
+					writer.writeUTF("File " + file.getAbsolutePath() + " is created!");
+					writer.flush();
+					bos.flush();
+					fos.flush();
+					bos.close();
+					fos.close();
+					out.flush();
+
+				} else file.mkdirs();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void main(String[] args){
+		int port = Integer.parseInt(args[0]);
+		Server server = new Server(port);
+		while(true)
+			server.run();
+	}
+}
